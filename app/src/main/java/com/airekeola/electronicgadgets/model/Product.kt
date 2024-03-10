1bac5d3d4e1bb4b01ac154166cb52ad52083fc3f
Bazz <abbasirekeola@gmail.com>
2024-03-10 11:26:31 -0500

    assignment 5

---
+++ app/src/main/java/com/airekeola/electronicgadgets/model/Product.kt
@@ -0,0 +1,5 @@
+package com.airekeola.electronicgadgets.model
+
+import java.io.Serializable
+
+data class Product(val productName: String, val productDescription: String, val cost: Double):Serializable
